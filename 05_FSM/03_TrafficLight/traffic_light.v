3892d99a50ef37a50582a728592814738f99e616
Ritesh Kumar <rkharwal2006@gmail.com>
2026-06-20 22:05:53 +0530

    ...

--- 05_FSM/03_TrafficLight/traffic_light.v
+++ 05_FSM/03_TrafficLight/traffic_light.v
@@ -24,27 +24,30 @@ module traffic_light#(
     parameter GREEN_MAX = 6'd54,
     parameter CYCLE_MAX = 6'd59
     )(
-    input clk,rst,
-    input t1,t2,t3,t4,
-    input e1, e2, e3, e4,
-    output r1,r2,r3,r4,
-    output reg g1,g2,g3,g4,
-    output reg y1,y2,y3,y4
+    input clk, rst,
+    input t1, t2, t3, t4,
+    input e1, e2, e3, e4, 
+    input night_mode,
+    output r1, r2, r3, r4,
+    output reg g1, g2, g3, g4,
+    output reg y1, y2, y3, y4
     );
 
-    reg [5:0]count ;
-    reg [1:0]side ;
+    reg [5:0] count;
+    reg [1:0] side;
+    
     reg t_active;
-    reg e_active;
-    wire side_trigger ;
+    reg e_active; 
+    
+    wire side_trigger;
     wire any_traffic;
-    wire any_emergency;
+    wire any_emergency; 
     
     assign any_traffic = (t1 | t2 | t3 | t4);
     assign any_emergency = (e1 | e2 | e3 | e4);
 
-    // Multiplexer (Input Routing)
-    always@(*)begin
+    // Multiplexers (Input Routing)
+    always @(*) begin
         case(side)
             2'b00 : begin t_active = t1; e_active = e1; end
             2'b01 : begin t_active = t2; e_active = e2; end
@@ -55,64 +58,66 @@ module traffic_light#(
     end
     
     // Timer State Machine (Datapath)
-    always@(posedge clk or posedge rst)begin
-        if(rst) count <= 6'd0 ;
-        else    begin
-                    if(count < GREEN_MAX)
-                        begin
-                            if (any_emergency & ~e_active)  count <= GREEN_MAX;
-                            else if(t_active | e_active) count <= count + 1; 
-                            else if(any_traffic) count <= GREEN_MAX;
-                            else count <= count;
-                        end
-                    else if(count < CYCLE_MAX)
-                        begin
-                            count <= count + 1 ;
-                        end
-                    else count <= 6'd0 ;
+    always @(posedge clk or posedge rst) begin
+        if(rst) count <= 6'd0;
+        else if (night_mode) count <= 6'd0;
+        else begin
+            if(count < GREEN_MAX) begin
+                if (any_emergency & ~e_active) begin
+                    count <= GREEN_MAX; 
                 end
+                else if(t_active | e_active) count <= count + 1; 
+                else if(any_traffic) count <= GREEN_MAX;
+                else count <= count; 
+            end
+            else if(count < CYCLE_MAX) begin
+                count <= count + 1; 
+            end
+            else count <= 6'd0;
+        end
     end
 
-    assign side_trigger = (count == CYCLE_MAX) ;
+    assign side_trigger = (count == CYCLE_MAX);
 
     // Side State Machine (Control Logic)
-    always@(posedge clk or posedge rst)begin
-        if(rst) side <= 2'b00 ;
-        else if(side_trigger)begin
-                    if (e1) side <= 2'b00;
-                    else if (e2) side <= 2'b01;
-                    else if (e3) side <= 2'b10;
-                    else if (e4) side <= 2'b11;
-                    else begin
-                        case(side)
-                            2'b00: begin
-                                if(t2) side <= 2'b01;
-                                else if(t3) side <= 2'b10;
-                                else if(t4) side <= 2'b11;
-                                else side <= 2'b00;
-                            end
-                            2'b01: begin
-                                if(t3) side <= 2'b10;
-                                else if(t4) side <= 2'b11;
-                                else if(t1) side <= 2'b00;
-                                else side <= 2'b01;
-                            end
-                            2'b10: begin
-                                if(t4) side <= 2'b11;
-                                else if(t1) side <= 2'b00;
-                                else if(t2) side <= 2'b01;
-                                else side <= 2'b10;
-                            end
-                            2'b11: begin
-                                if(t1) side <= 2'b00;
-                                else if(t2) side <= 2'b01;
-                                else if(t3) side <= 2'b10;
-                                else side <= 2'b11;
-                            end
-                            default: side <= 2'b00;                    
-                        endcase
+    always @(posedge clk or posedge rst) begin
+        if(rst) side <= 2'b00;
+        else if(night_mode) side <= 2'b00;
+        else if(side_trigger) begin
+            if (e1) side <= 2'b00;
+            else if (e2) side <= 2'b01;
+            else if (e3) side <= 2'b10;
+            else if (e4) side <= 2'b11;
+            else begin
+                case(side)
+                    2'b00: begin
+                        if(t2) side <= 2'b01;
+                        else if(t3) side <= 2'b10;
+                        else if(t4) side <= 2'b11;
+                        else side <= 2'b00;
                     end
-                 end
+                    2'b01: begin
+                        if(t3) side <= 2'b10;
+                        else if(t4) side <= 2'b11;
+                        else if(t1) side <= 2'b00;
+                        else side <= 2'b01;
+                    end
+                    2'b10: begin
+                        if(t4) side <= 2'b11;
+                        else if(t1) side <= 2'b00;
+                        else if(t2) side <= 2'b01;
+                        else side <= 2'b10;
+                    end
+                    2'b11: begin
+                        if(t1) side <= 2'b00;
+                        else if(t2) side <= 2'b01;
+                        else if(t3) side <= 2'b10;
+                        else side <= 2'b11;
+                    end
+                    default: side <= 2'b00;                    
+                endcase
+            end
+        end
     end
     
     // Output Logic Decoder
@@ -123,29 +128,25 @@ module traffic_light#(
     always @(*) begin
         g1 = 0; g2 = 0; g3 = 0; g4 = 0;
         y1 = 0; y2 = 0; y3 = 0; y4 = 0;
-        case (side)
-            2'b00: begin
-                g1 = is_green;
-                y1 = is_yellow;
-            end
-            2'b01: begin
-                g2 = is_green;
-                y2 = is_yellow;
-            end
-            2'b10: begin
-                g3 = is_green;
-                y3 = is_yellow;
-            end
-            2'b11: begin
-                g4 = is_green;
-                y4 = is_yellow;
-            end
-        endcase
+        
+        if (night_mode) begin
+            y1 = clk;
+            y2 = clk; 
+            y3 = clk;
+            y4 = clk;
+        end else begin
+            case (side)
+                2'b00: begin g1 = is_green; y1 = is_yellow; end
+                2'b01: begin g2 = is_green; y2 = is_yellow; end
+                2'b10: begin g3 = is_green; y3 = is_yellow; end
+                2'b11: begin g4 = is_green; y4 = is_yellow; end
+            endcase
+        end
     end
 
-    assign r1 = ~(g1 | y1);
-    assign r2 = ~(g2 | y2);
-    assign r3 = ~(g3 | y3);
-    assign r4 = ~(g4 | y4);
+    assign r1 = night_mode ? 1'b0 : ~(g1 | y1);
+    assign r2 = night_mode ? 1'b0  : ~(g2 | y2);
+    assign r3 = night_mode ? 1'b0 : ~(g3 | y3);
+    assign r4 = night_mode ? 1'b0  : ~(g4 | y4);
     
 endmodule
--- 05_FSM/03_TrafficLight/traffic_light_tb.v
+++ 05_FSM/03_TrafficLight/traffic_light_tb.v
@@ -1,30 +1,11 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.06.2026 20:54:56
-// Design Name: 
-// Module Name: traffic_light_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`timescale 1s / 100ms
 
-
-module traffic_light_tb;
+module tb_traffic_light;
 
     reg clk, rst;
     reg t1, t2, t3, t4;
     reg e1, e2, e3, e4;
+    reg night_mode;
 
     wire r1, r2, r3, r4;
     wire g1, g2, g3, g4;
@@ -37,6 +18,7 @@ module traffic_light_tb;
         .clk(clk), .rst(rst),
         .t1(t1), .t2(t2), .t3(t3), .t4(t4),
         .e1(e1), .e2(e2), .e3(e3), .e4(e4),
+        .night_mode(night_mode),
         .r1(r1), .r2(r2), .r3(r3), .r4(r4),
         .g1(g1), .g2(g2), .g3(g3), .g4(g4),
         .y1(y1), .y2(y2), .y3(y3), .y4(y4)
@@ -48,31 +30,28 @@ module traffic_light_tb;
         clk = 0; rst = 1; 
         t1 = 0; t2 = 0; t3 = 0; t4 = 0;
         e1 = 0; e2 = 0; e3 = 0; e4 = 0;
-        
-        #2 rst = 0; 
-        
+        night_mode = 0;
+        #2 rst = 0;
         t1 = 1; t2 = 1; t3 = 1; t4 = 1;
         #30; 
-        
         e3 = 1; 
-        
         #20;    
         e3 = 0; 
         #15;
-
         t1 = 0; t2 = 0; t3 = 0; t4 = 0;
-        
         #30; 
-
         t1 = 1; 
         #20; 
-
+        night_mode = 1;
+        #10;
+        night_mode = 0;
+        #10;
         $finish; 
     end
 
     initial begin
-        $monitor("ActiveSide=%b | Timer=%0d | Emer=%b%b%b%b | Traf=%b%b%b%b | Lights(G Y R) S1:%b%b%b S2:%b%b%b S3:%b%b%b S4:%b%b%b", 
-                 uut.side, uut.count, 
+        $monitor("NightMode=%b | ActiveSide=%b | Timer=%0d | Emer=%b%b%b%b | Traf=%b%b%b%b | Lights(G Y R) S1:%b%b%b S2:%b%b%b S3:%b%b%b S4:%b%b%b", 
+                 night_mode, uut.side, uut.count, 
                  e1, e2, e3, e4, 
                  t1, t2, t3, t4, 
                  g1, y1, r1,  g2, y2, r2,  g3, y3, r3,  g4, y4, r4);
